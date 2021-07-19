bdf49823ea453b5ded4b9508ac82528412956df8
QwiglyDee <qwiglydee@gmail.com>
2021-07-19 19:37:42 +0700

    recording data

--- captcha/__init__.py
+++ captcha/__init__.py
@@ -1,4 +1,4 @@
-from otree import api
+from otree.api import *
 
 from . import utils
 
@@ -7,7 +7,7 @@ Experimental catcha game
 """
 
 
-class Constants(api.BaseConstants):
+class Constants(BaseConstants):
     name_in_url = 'captcha'
     players_per_group = None
     num_rounds = 1
@@ -15,23 +15,49 @@ class Constants(api.BaseConstants):
     default_captcha_length = 5
 
 
-class Subsession(api.BaseSubsession):
+class Subsession(BaseSubsession):
     pass
 
 
-class Group(api.BaseGroup):
+class Group(BaseGroup):
     pass
 
 
-class Player(api.BasePlayer):
-    pass
+class Player(BasePlayer):
+    total_puzzles = models.IntegerField()
+    total_solved = models.IntegerField()
 
 
-# customizable functions
+# puzzle-specific stuff
+
+class PuzzleGame(ExtraModel):
+    """A model to store current game state"""
+    player = models.Link(Player)
+
+    elapsed = models.FloatField(initial=0)
+    iteration = models.IntegerField(initial=0)
+    difficulty = models.FloatField(initial=0)
+    puzzle = models.StringField(initial="")
+    solution = models.StringField(initial="")
+
+
+class PuzzleRecord(ExtraModel):
+    """A model to keep record of all solved puzzles"""
+    player = models.Link(Player)
+    elapsed = models.FloatField()
+    iteration = models.IntegerField()
+    difficulty = models.FloatField()
+    puzzle = models.StringField()
+    solution = models.StringField()
+    answer = models.StringField()
+    is_correct = models.BooleanField()
+
 
 def generate_puzzle(player: Player):
     difficulty = player.session.config.get('captcha_length', Constants.default_captcha_length)
-    return difficulty, utils.generate_text(difficulty)
+    text = utils.generate_text(difficulty)
+    # difficulty, puzzle, solution
+    return difficulty, text, text
 
 
 def generate_image(text):
@@ -40,8 +66,45 @@ def generate_image(text):
     return image
 
 
+def check_answer(solution: str, answer: str):
+    return answer.upper() == solution
+
+
+# generic game function, independent from above specific
+
 def play_captcha(player: Player, data: dict):
-    difficulty, puzzle = generate_puzzle(player)
+    """Handles iteration of the game"""
+    # create or retrieve current state
+    if 'start' in data:
+        current = PuzzleGame.create(player=player)
+    else:
+        current = PuzzleGame.objects_get(player=player)
+
+    # record current answer
+    if 'answer' in data:
+        answer = data['answer']
+        PuzzleRecord.create(
+            player=player,
+            elapsed=current.elapsed,
+            iteration=current.iteration,
+            difficulty=current.difficulty,
+            puzzle=current.puzzle,
+            solution=current.solution,
+            answer=answer,
+            is_correct=check_answer(current.solution, answer)
+        )
+
+    # generate next puzzle
+    difficulty, puzzle, solution = generate_puzzle(player)
+
+    # save current puzzle
+    current.difficulty = difficulty
+    current.puzzle = puzzle
+    current.solution = solution
+    current.iteration += 1
+    # current.elapsed = ...
+
+    # send the puzzle as image
     image = generate_image(puzzle)
     data = utils.encode_image(image)
     return {player.id_in_group: {'image': data}}
@@ -49,14 +112,22 @@ def play_captcha(player: Player, data: dict):
 
 # PAGES
 
-
-class MainPage(api.Page):
+class MainPage(Page):
     timeout_seconds = 60
 
     live_method = play_captcha
 
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        current = PuzzleGame.object_get(player=player)
+        # record total player stats
+        player.total_puzzles = current.iteration
+        player.total_solved = len(PuzzleRecord.filter(player=player, is_correct=True))
+        # clean up
+        current.delete()
 
-class Results(api.Page):
+
+class Results(Page):
     pass
 
 
