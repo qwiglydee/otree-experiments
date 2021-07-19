99f5cf8d872dfd4bf48fb467d6e89226584256e4
QwiglyDee <qwiglydee@gmail.com>
2021-07-19 14:38:06 +0700

    setup

---
+++ README.md
@@ -0,0 +1,32 @@
+# oTree experiments
+
+Some experimental implementation of various games for oTree v5
+
+- captcha
+
+## Development
+
+0. clone this repo into working directory
+   ```bash
+   git clone ...
+   cd ...
+   ```
+1. create and activate virtualenv in working directory
+   ```bash
+   python -m venv .venv
+   source .venv.bin/activate
+   pip install --upgrade pip
+   ```
+2. install requirements
+   ```bash
+   pip install -r requirements.txt
+   pip install -r requirements.devel.txt
+   ```
+3. run the server
+   ```bash
+   otree devserver
+   ```
+
+## Integration into your code
+
+TODO
---
+++ settings.py
@@ -0,0 +1,32 @@
+from os import environ
+
+SESSION_CONFIGS = [
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(
+    real_world_currency_per_point=1.00, participation_fee=0.00, doc=""
+)
+
+PARTICIPANT_FIELDS = []
+SESSION_FIELDS = []
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'USD'
+USE_POINTS = True
+
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """ """
+
+SECRET_KEY = '2015765205890'
