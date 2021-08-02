71178b394bf784e88bfb849d00a6b302a1d73234
oTree-org <chris@otree.org>
2021-08-02 13:25:15 -0600

    Pillow warning message

--- real_effort/image_utils.py
+++ real_effort/image_utils.py
@@ -4,6 +4,13 @@ Utilities to generate and manipulate images
 from io import BytesIO
 from base64 import b64encode
 
+MSG_NEED_PIL = """
+FAILURE: Before using these real-effort tasks,
+You need to: 
+(1) run "pip install Pillow"
+(2) add Pillow to your requirements.txt
+"""
+
 try:
     # PIL is not actually used here; this is just to generate the warning to install Pillow.
     import PIL  # noqa
@@ -11,9 +18,7 @@ except ImportError:
     import sys
 
     sys.tracebacklimit = 0
-    raise SystemExit(
-        f"FAILURE: Missing imaging library required in `{__name__}`\nYou need to install it using `pip install Pillow`"
-    )
+    raise SystemExit(MSG_NEED_PIL)
 
 
 def encode_image(image):
