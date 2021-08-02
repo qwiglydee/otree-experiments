311fed71b94cd77fa68cbdbc67c259458faa0d90
QwiglyDee <qwiglydee@gmail.com>
2021-08-02 19:39:54 +0700

    start

---
+++ iat/__init__.py
@@ -0,0 +1,40 @@
+from otree.api import *
+
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'iat'
+    players_per_group = None
+    num_rounds = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    pass
+
+
+# PAGES
+class MyPage(Page):
+    pass
+
+
+class ResultsWaitPage(WaitPage):
+    pass
+
+
+class Results(Page):
+    pass
+
+
+page_sequence = [MyPage, ResultsWaitPage, Results]
