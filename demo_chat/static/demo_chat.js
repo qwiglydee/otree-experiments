22252cfc76eca23e0bdd6bb54a50875da7cbdd3f
QwiglyDee <qwiglydee@gmail.com>
2022-02-06 00:56:32 +0700

    updated chat

--- demo_chat/static/demo_chat.js
+++ demo_chat/static/demo_chat.js
@@ -1,4 +1,5 @@
-liveRecv = otree.live_utils.livePageRecv;
+// the default handler generates 'ot.live.*' events
+liveRecv = otree.live_utils.liveDefaultRecv;
 
 function main() {
   let page = otree.page;
@@ -16,53 +17,64 @@ function main() {
 
   function addMessage(cls, txt, src, dst) {
     chat.push({ class: cls, text: txt, source: src, dest: dst });
-    chat = chat.slice(-maxlog);
+    chat = chat.slice(-maxlog); // cut down because number of lines is fixed
     page.update({ chat: chat });
   }
 
   function resetInput() {
     recipient = undefined;
-    page.reset(['text', 'recipient']);
+    page.reset(["text", "recipient"]);
   }
+
+  //// events from liveRecv
 
   page.onEvent("ot.live.joined", function (event, data) {
     console.debug("joined", data);
     loadParty(data.party);
-    addMessage('system', `Welcome, ${data.newcomer}!`);
+    addMessage("system", `Welcome, ${data.newcomer}!`);
   });
 
   page.onEvent("ot.live.left", function (event, data) {
     console.debug("left", data);
     loadParty(data.party);
-    addMessage('system', `Bye, ${data.nickname}!`);
+    addMessage("system", `Bye, ${data.nickname}!`);
   });
 
   page.onEvent("ot.live.talk", function (event, data) {
     console.debug("talk", data);
-    addMessage('talk', data.text, data.source);
+    addMessage("talk", data.text, data.source);
   });
 
   page.onEvent("ot.live.wisper", function (event, data) {
     console.debug("wisper", data);
-    addMessage('wisper', data.text, data.source, mynickname);
+    addMessage("wisper", data.text, data.source, mynickname);
+  });
+
+  //// events from buttons
+
+  page.onEvent("chat.join", function () {
+    page.submitInputs('nickname');
   });
 
   page.onEvent("chat.clear", function () {
     recipinent = null;
-    page.reset(['text', 'recipient']);
+    page.reset(["text", "recipient"]);
   });
 
   page.onEvent("chat.send", function () {
-    page.submitInputs('text');
+    page.submitInputs("text");
   });
 
   page.onEvent("chat.leave", function () {
     liveSend({ type: "leave" });
-    page.update({ stage: "left" });
+    page.update({ stage: "leaving" });
     otree.utils.timers.delay(() => page.submit(), 3000);
-});
+  });
 
-  page.onInput = function (name, value) {
+  //// events from inputs
+
+  page.onEvent("ot.input", function (event, data) {
+    let { name, value } = data;
     console.debug("input", name, value);
 
     if (name == "nickname") {
@@ -72,20 +84,22 @@ function main() {
     }
 
     if (name == "text") {
-      if (recipient != undefined ) {
+      if (recipient != undefined) {
         liveSend({ type: "wispering", text: value, dest: recipient });
-        addMessage('wisper', value, mynickname, recipient);
+        addMessage("wisper", value, mynickname, recipient);
       } else {
         liveSend({ type: "saying", text: value });
       }
       resetInput();
     }
 
-    if (name == 'recipient') {
+    if (name == "recipient") {
       recipient = party[value];
       page.update({ recipient: recipient });
     }
-  };
+  });
+
+  //// main start
 
   page.update({ stage: "joining" });
 }
